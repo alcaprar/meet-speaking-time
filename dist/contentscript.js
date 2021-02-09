9c5d53f7ed71f3a402581583eca5147f6e23ae3c
Alessandro Caprarelli <caprarelli.alessandro@gmail.com>
2021-02-09 11:30:19 +0100

    init previous prototype and started TS project

---
+++ dist/contentscript.js
@@ -0,0 +1,9 @@
+/******/ (() => { // webpackBootstrap
+/*!********************************************!*\
+  !*** ./src/contentscript/contentscript.ts ***!
+  \********************************************/
+console.log("Ciao");
+
+/******/ })()
+;
+//# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJzb3VyY2VzIjpbIndlYnBhY2s6Ly9tZWV0LXRhbGstdGltZS1yZWNvcmRlci8uL3NyYy9jb250ZW50c2NyaXB0L2NvbnRlbnRzY3JpcHQudHMiXSwibmFtZXMiOltdLCJtYXBwaW5ncyI6Ijs7OztBQUFBIiwiZmlsZSI6ImNvbnRlbnRzY3JpcHQuanMiLCJzb3VyY2VzQ29udGVudCI6WyJjb25zb2xlLmxvZyhcIkNpYW9cIik7XG4iXSwic291cmNlUm9vdCI6IiJ9

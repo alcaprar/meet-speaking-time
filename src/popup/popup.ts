3f4984cc443dad4be3652a10e161e8b32d5a816c
matteo.tritto <matteo.tritto@weroad.it>
2021-02-14 13:17:36 +0100

    - popup layout trello's like

--- src/popup/popup.ts
+++ src/popup/popup.ts
@@ -4,7 +4,16 @@ chrome.storage.sync.get(["participants"], function(items){
 });
 
 function makeTableHTML(ar) {
-  return `<table>${ar.reduce((c, o) => c += `<tr>${o.reduce((c, d) => (c += `<td>${d}</td>`), '')}</tr>`, '')}</table>`
+  return `${ar.reduce((c, o) => c += `<div class="bg-white p-2 flex items-center rounded mt-1 border-b border-grey cursor-pointer hover:bg-gray-100">
+                                        <img src="https://i.imgur.com/OZaT7jl.png" class="rounded-full mr-2" />
+                                        <div class="flex flex-col w-full">
+                                          <span>${o[0]}</span>
+                                          <div class="flex items-center justify-between">
+                                            <span>${o[1]}</span>
+                                            <span>${o[2]}</span>
+                                          </div>
+                                        </div>
+                                     </div>`, '')}`
 }
 
 function formatParticipants (participants) : string {

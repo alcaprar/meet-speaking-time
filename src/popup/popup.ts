316846a316654877e1cc9ef0b1bfd0576c37d9fe
matteo.tritto <matteo.tritto@weroad.it>
2021-02-19 21:59:22 +0100

    vue porting

--- src/popup/popup.ts
+++ src/popup/popup.ts
@@ -1,39 +1,37 @@
-import { formatTime} from "../Utils"
-import {MeetingInformation, Storage} from '../Storage'
-const storage = new Storage();
+import { formatTime } from '../Utils'
+import { MeetingInformation, Storage } from '../Storage'
+const storage = new Storage()
 
-const updateView = function () {
-  storage.getCurrent(function (currentMeeting : MeetingInformation) {
-    document.querySelector("#table").innerHTML = formatParticipants(currentMeeting.participants)
-    document.querySelector("#totalTime").innerHTML = formatTime(currentMeeting.elapsed, false);
-  })
-
-  // TODO there should be also the history object
-}
-
-setInterval(function (){
-  updateView();
-}, 1000)
-
-function makeTableHTML(ar) {
-  return `${ar.reduce((c, o) => c += `<div class="bg-white p-2 flex items-center rounded mt-1 border-b border-grey cursor-pointer hover:bg-gray-100">
-                                        <img src="${o[3]}" class="rounded-full mr-2" width="24px" height="24px" />
-                                        <div class="flex flex-col w-full">
-                                          <span>${o[0]}</span>
-                                          <div class="flex items-center justify-between">
-                                            <span>${o[1]}</span>
-                                            <span>${o[2]}</span>
-                                          </div>
-                                        </div>
-                                     </div>`, '')}`
-}
-
-function formatParticipants (participants) : string {
-  return makeTableHTML(participants);
-}
-
-document
-  .getElementById('create-meeting')
-  .addEventListener('click', function () {
-    chrome.runtime.sendMessage({ createMeeting: true })
-  });
+new Vue({
+  el: '#app',
+  data () {
+    return {
+      participants: [],
+      elapsed: null,
+      _data: null,
+    }
+  },
+  mounted () {
+    const _self = this
+    setInterval(() => {
+      _self.updateView()
+    }, 1000)
+  },
+  computed: {
+  },
+  methods: {
+    async createMeeting () {
+      const createMeeting = await chrome.runtime.sendMessage({ createMeeting: true })
+      console.log('createMeeting', createMeeting)
+    },
+    async updateView () {
+      const _self = this
+      storage.getCurrent(function (currentMeeting: MeetingInformation) {
+        _self.participants = currentMeeting.participants
+        _self.elapsed = formatTime(currentMeeting.elapsed, false)
+        _self._data = currentMeeting
+      })
+      // TODO there should be also the history object
+    },
+  }
+})
